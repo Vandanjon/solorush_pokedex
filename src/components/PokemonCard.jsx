5c66f1c33402e3530fc23431e17703fa60efe289
Benoît Vandanjon <benoit@vandanjon.fr>
2024-03-29 11:02:09 +0100

    exo done

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,4 +1,5 @@
-/* eslint-disable react/prop-types */
+import PropTypes from "prop-types";
+
 function PokemonCard({ pokemon }) {
   return (
     <figure>
@@ -11,5 +12,12 @@ function PokemonCard({ pokemon }) {
     </figure>
   );
 }
+
+PokemonCard.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imgSrc: PropTypes.string,
+  }).isRequired,
+};
 
 export default PokemonCard;
